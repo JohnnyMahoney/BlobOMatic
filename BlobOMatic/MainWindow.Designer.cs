2498fb7cd45cac12471e9ee7a8ee94e600d8e97e
JohnnyMahoney <c.j.a.hermanns@gmail.com>
2019-12-07 23:39:40 +0100

    Forms Versuch gestartet

---
+++ BlobOMatic/MainWindow.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace BlobOMatic
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.gamePanel.Location = new System.Drawing.Point(280, 150);
+            this.gamePanel.Margin = new System.Windows.Forms.Padding(0);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(720, 400);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.gamePanel_Paint);
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.gamePanel);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "mainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blob \'o Matic";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+    }
+}
+
---
+++ BlobOMatic/graphics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlobOMatic
+{
+    static class graphics
+    {
+        public static void DrawCircles(Panel panel)
+        {
+            var sx = panel.Width;
+            var sy = panel.Height;
+
+            var deltax = 20;
+            var deltay = 20;
+
+            for (int i = 0; i < sx; i += deltax)
+            {
+                for (int j = 0; j < sy; j += deltay)
+                {
+                    System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+                    System.Drawing.Graphics formGraphics;
+                    formGraphics = panel.CreateGraphics();
+                    formGraphics.DrawEllipse(new Pen(myBrush), i, j, deltax, deltay);
+                    myBrush.Dispose();
+                    formGraphics.Dispose();
+                }
+            }
+
+
+        }
+    }
+}
