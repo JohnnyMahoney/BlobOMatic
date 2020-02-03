c1b198daae6fda8919899b9afdb15a66fb872f07
JohnnyMahoney <c.j.a.hermanns@gmail.com>
2020-02-03 01:40:08 +0100

    Neuanfang

---
+++ BlobOMatic/Blob.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace BlobOMatic
+{
+    class Blob
+    {
+        public Ellipse Ellipse { get; set; }
+        public int Value { get; set; }
+        public bool Aktiv { get; set; }
+
+
+        public Blob()
+        {
+            Value = 0;
+            Aktiv = false;
+            Ellipse = new Ellipse
+            {
+                Width = 100,
+                Height = 100,
+                Stroke = Brushes.Black,
+                Fill = Brushes.LightGray
+            };
+        }
+
+        
+    }
+}
---
+++ BlobOMatic/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlobOMatic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int x = (int)mainCanvas.Width / 100;
+            int y = (int)mainCanvas.Height / 100;
+
+            CreateEmptyBlobs(x, y, out Blob[,] blobs);
+
+        }
+
+        void CreateEmptyBlobs(int cols, int rows, out Blob[,] blobs)
+        {
+            blobs = new Blob[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < cols; column++)
+                {
+                    Blob _ = new Blob();
+                    
+                    blobs[row, column] = _;
+
+                    mainCanvas.Children.Add(_.Ellipse);
+                    Canvas.SetTop(_.Ellipse, row * 100);
+                    Canvas.SetLeft(_.Ellipse, column * 100);
+                    
+                }
+            }
+
+        }
+    }
+}
